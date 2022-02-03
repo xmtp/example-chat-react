d6bebb1a5c40c227a889980ff172bcb67faa40d2
Steven Normore <snormore@gmail.com>
2022-02-03 07:15:34 -0500

    Set webpack.resolve.fallback.fs = false in next.config.js for node-localstorage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
