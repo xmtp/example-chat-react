3312b3d5ea3edb9084f0c2ec9a3305ad77d0af23
Steven Normore <snormore@gmail.com>
2022-02-04 10:22:31 -0500

    Initial wallets integration via web3modal

---
+++ components/App.tsx
@@ -0,0 +1,19 @@
+import { XmtpProvider } from "../components/XmtpContext";
+import Layout from "../components/Layout";
+import { WalletProvider } from "./WalletContext";
+
+type AppProps = {
+  children?: React.ReactNode;
+};
+
+function App({ children }: AppProps) {
+  return (
+    <WalletProvider>
+      <XmtpProvider>
+        <Layout>{children}</Layout>
+      </XmtpProvider>
+    </WalletProvider>
+  );
+}
+
+export default App;
