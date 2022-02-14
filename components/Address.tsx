aa0105c5100d60b1bf93e48379c5107cbaa5a1f8
Nick Molnar <nmolnar@brex.com>
2022-02-14 07:04:09 -0800

    Import classNames

--- components/Address.tsx
+++ components/Address.tsx
@@ -1,11 +1,9 @@
+import { classNames } from '../helpers';
+
 type AddressProps = {
 	address: string;
 	className?: string;
 };
-
-function classNames(...classes: string[]) {
-	return classes.filter(Boolean).join(' ');
-}
 
 const shortAddress = (addr: string): string =>
 	addr.length > 10 && addr.startsWith('0x') ? `${addr.substring(0, 6)}...${addr.substring(addr.length - 4)}` : addr;
