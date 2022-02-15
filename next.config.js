3d8f4f531566cc265492e2eda6c37c26955a4e3f
Nick Molnar <nmolnar@brex.com>
2022-02-14 16:13:14 -0800

    Add plugin to import image

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
+import optimizedImages from 'next-optimized-images'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = optimizedImages({
   reactStrictMode: true,
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.resolve.fallback.fs = false
     }
-    return config;
+    return config
   },
-};
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
