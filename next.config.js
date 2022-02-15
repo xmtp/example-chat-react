6928da3c0bb00915fef29333f319175f67d93da4
Nick Molnar <nmolnar@brex.com>
2022-02-14 16:22:54 -0800

    Fix next build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-import optimizedImages from 'next-optimized-images'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withOptimizedImages = require('next-optimized-images')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = optimizedImages({
+const nextConfig = withOptimizedImages({
   reactStrictMode: true,
-
+  images: {
+    disableStaticImages: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false
