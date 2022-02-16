5d2f5aaa72381a1304c2217bc4efc07421e53bf0
Nick Molnar <nmolnar@brex.com>
2022-02-15 22:26:04 -0800

    Remove comment

--- components/Sidebar/ConversationsList.tsx
+++ components/Sidebar/ConversationsList.tsx
@@ -83,35 +83,6 @@ const ConversationTile = ({
   )
 }
 
-/**
- * 
-return (
-            <Link href={path} key={convo.peerAddress}>
-              <a
-                className={classNames(
-                  isCurrentPath
-                    ? 'bg-gray-100 text-gray-900'
-                    : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                )}
-              >
-                <InboxInIcon
-                  className={classNames(
-                    isCurrentPath
-                      ? 'text-gray-500'
-                      : 'text-gray-400 group-hover:text-gray-500',
-                    'mr-3 flex-shrink-0 h-6 w-6'
-                  )}
-                  aria-hidden="true"
-                />
-                <Address address={convo.peerAddress} />
-              </a>
-            </Link>
-          )
-        })
- * @returns 
- */
-
 const ConversationsList = ({
   conversations,
 }: ConversationListProps): JSX.Element => {
