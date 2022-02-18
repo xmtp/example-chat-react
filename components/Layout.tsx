a4aa6fdefa88e131d2b96478797884eaa3b20ee7
Saul Carlin <saul.carlin@gmail.com>
2022-02-17 17:26:57 -0800

    move viewport meta to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -125,6 +125,10 @@ const Layout: React.FC = ({ children }) => {
     <>
       <Head>
         <title>Chat via XMTP</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </Head>
       <div>
         <NavigationView>
