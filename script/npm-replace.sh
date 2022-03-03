c3295b85ef6219a49e603efc120a2c06723befef
Nick Molnar <nmolnar@brex.com>
2022-03-02 16:36:58 -0800

    Add replacement script

---
+++ script/npm-replace.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This is an awful hack, but we only need it for a few weeks until we make the npm package public
+# Required for GH Actions and CloudFlare Pages
+
+set -e
+
+if [[ -z "$XMTP_GH_TOKEN" ]]; then
+  echo "Not replacing package.json"
+  exit 0
+else
+  sed -i '' "s/github:xmtp\/xmtp-js/git+https:\/\/$XMTP_GH_TOKEN:x-oauth-basic@github.com\/xmtp\/xmtp-js.git/g" package.json
+fi
