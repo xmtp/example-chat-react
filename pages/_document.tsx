e73310775231569f1d421f23b0a91ca70d6ccf7b
Anoop Ranganath <anoop@xmtp.com>
2022-04-23 13:29:25 -0500

    Change icon and link to also use relative URLs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class AppDocument extends Document {
       <Html className="h-full bg-gray-100">
         <Head>
           <meta name="description" content="Chat via XMTP" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="./favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Inter:400,500,600,700|Inconsolata:400,600,700&display=swap"
             rel="stylesheet"
