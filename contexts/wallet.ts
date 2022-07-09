388a27ae04559c06b761a6a810a19c8faecbe08c
0xlws <87901794+0xlws@users.noreply.github.com>
2022-07-09 12:48:22 +0200

    autosign metamask messages

--- contexts/wallet.ts
+++ contexts/wallet.ts
@@ -1,17 +1,23 @@
 import { createContext } from 'react'
-import { ethers, Signer } from 'ethers'
+import {
+  ethers,
+  // Signer,
+  Wallet,
+} from 'ethers'
 import Web3Modal from 'web3modal'
 
 export type WalletContextType = {
   provider: ethers.providers.Web3Provider | undefined
-  signer: Signer | undefined
+  // signer: Signer | undefined
+  signer: Wallet | undefined
   address: string | undefined
-  chainId: number | undefined
   web3Modal: Web3Modal | undefined
+  chainId: number | undefined
   resolveName: (name: string) => Promise<string | undefined>
   lookupAddress: (address: string) => Promise<string | undefined>
   getAvatarUrl: (address: string) => Promise<string | undefined>
-  connect: () => Promise<Signer | undefined>
+  // connect: () => Promise<Signer | undefined>
+  connect: () => Promise<Wallet | undefined>
   disconnect: () => Promise<void>
 }
 
