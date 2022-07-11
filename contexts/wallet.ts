1b1b1f7597d84a22cc87d55b8d6184702609edb8
0xlws <87901794+0xlws@users.noreply.github.com>
2022-07-11 04:44:12 +0200

    refactor autosign

--- contexts/wallet.ts
+++ contexts/wallet.ts
@@ -1,23 +1,17 @@
 import { createContext } from 'react'
-import {
-  ethers,
-  // Signer,
-  Wallet,
-} from 'ethers'
+import { ethers, Signer, Wallet } from 'ethers'
 import Web3Modal from 'web3modal'
 
 export type WalletContextType = {
   provider: ethers.providers.Web3Provider | undefined
-  // signer: Signer | undefined
-  signer: Wallet | undefined
+  signer: Signer | Wallet | undefined
   address: string | undefined
   web3Modal: Web3Modal | undefined
   chainId: number | undefined
   resolveName: (name: string) => Promise<string | undefined>
   lookupAddress: (address: string) => Promise<string | undefined>
   getAvatarUrl: (address: string) => Promise<string | undefined>
-  // connect: () => Promise<Signer | undefined>
-  connect: () => Promise<Wallet | undefined>
+  connect: (autosign: boolean) => Promise<Signer | Wallet | undefined>
   disconnect: () => Promise<void>
 }
 
