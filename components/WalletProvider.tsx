213eaf9bb693afa0acdf4b15dc871e413b0a9468
0xlws <87901794+0xlws@users.noreply.github.com>
2022-07-11 04:41:21 +0200

    refactor autosign

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -1,9 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
-import {
-  ethers,
-  //  Signer
-  Wallet,
-} from 'ethers'
+import { ethers, Signer, Wallet } from 'ethers'
 import Web3Modal, { IProviderOptions, providers } from 'web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import WalletLink from 'walletlink'
@@ -11,6 +7,7 @@ import { WalletContext } from '../contexts/wallet'
 
 const ETH_CHAIN_ID = 1 // Ethereum mainnet
 const PRIVATE_KEY = `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`
+let autosign: boolean
 
 const cachedLookupAddress = new Map<string, string | undefined>()
 const cachedResolveName = new Map<string, string | undefined>()
@@ -24,8 +21,7 @@ export const WalletProvider = ({
   children,
 }: WalletProviderProps): JSX.Element => {
   const [provider, setProvider] = useState<ethers.providers.Web3Provider>()
-  // const [signer, setSigner] = useState<Signer>()
-  const [signer, setSigner] = useState<Wallet>()
+  const [signer, setSigner] = useState<Signer | Wallet>()
   const [web3Modal, setWeb3Modal] = useState<Web3Modal>()
   const [address, setAddress] = useState<string>()
   const [chainId, setChainId] = useState<number>()
@@ -77,6 +73,7 @@ export const WalletProvider = ({
     if (!web3Modal) return
     web3Modal.clearCachedProvider()
     localStorage.removeItem('walletconnect')
+    // localStorage.removeItem('autosign')
     Object.keys(localStorage).forEach((key) => {
       if (key.startsWith('-walletlink')) {
         localStorage.removeItem(key)
@@ -102,32 +99,42 @@ export const WalletProvider = ({
     [setChainId]
   )
 
-  const connect = useCallback(async () => {
-    if (!PRIVATE_KEY) {
-      throw new Error('try: rename .env.local.example to .env.local')
-    }
-
-    if (!web3Modal) throw new Error('web3Modal not initialized')
-    try {
-      const instance = await web3Modal.connect()
-      if (!instance) return
-      instance.on('accountsChanged', handleAccountsChanged)
-      const provider = new ethers.providers.Web3Provider(instance, 'any')
-      provider.on('network', handleChainChanged)
-      // const signer = provider.getSigner()
-      const signer = new Wallet(PRIVATE_KEY, provider)
-      const { chainId } = await provider.getNetwork()
-      setChainId(chainId)
-      setSigner(signer)
-      setAddress(await signer.getAddress())
-      return signer
-    } catch (e) {
-      // TODO: better error handling/surfacing here.
-      // Note that web3Modal.connect throws an error when the user closes the
-      // modal, as "User closed modal"
-      console.log('error', e)
-    }
-  }, [web3Modal, handleAccountsChanged, handleChainChanged])
+  const connect = useCallback(
+    async (autosignMsg: boolean) => {
+      if (autosignMsg && PRIVATE_KEY === 'undefined') {
+        throw new Error(
+          'NEXT_PUBLIC_PRIVATE_KEY inside ".env.local" not assigned, see ".env.local.example" (default: hardhat wallet)'
+        )
+      }
+      if (!web3Modal) throw new Error('web3Modal not initialized')
+      try {
+        const instance = await web3Modal.connect()
+        if (!instance) return
+        instance.on('accountsChanged', handleAccountsChanged)
+        const provider = new ethers.providers.Web3Provider(instance, 'any')
+        provider.on('network', handleChainChanged)
+        let signer
+        if (autosignMsg) {
+          localStorage.autosign = true
+          signer = new Wallet(PRIVATE_KEY, provider)
+        } else {
+          localStorage.autosign = false
+          signer = provider.getSigner()
+        }
+        const { chainId } = await provider.getNetwork()
+        setChainId(chainId)
+        setSigner(signer)
+        setAddress(await signer.getAddress())
+        return signer
+      } catch (e) {
+        // TODO: better error handling/surfacing here.
+        // Note that web3Modal.connect throws an error when the user closes the
+        // modal, as "User closed modal"
+        console.log('error', e)
+      }
+    },
+    [web3Modal, handleAccountsChanged, handleChainChanged]
+  )
 
   useEffect(() => {
     const infuraId =
@@ -171,10 +178,14 @@ export const WalletProvider = ({
   }, [])
 
   useEffect(() => {
-    if (PRIVATE_KEY === 'undefined')
+    if (localStorage.autosign != undefined) {
+      autosign = JSON.parse(localStorage.autosign)
+    }
+    if (autosign && PRIVATE_KEY === 'undefined') {
       throw new Error(
-        'rename ".env.local.example" file to ".env.local" to assign the private key (default: hardhat wallet)'
+        'NEXT_PUBLIC_PRIVATE_KEY inside ".env.local" not assigned, see ".env.local.example" (default: hardhat wallet)'
       )
+    }
     if (!web3Modal) return
     const initCached = async () => {
       const cachedProviderJson = localStorage.getItem(
@@ -187,8 +198,12 @@ export const WalletProvider = ({
       instance.on('accountsChanged', handleAccountsChanged)
       const provider = new ethers.providers.Web3Provider(instance, 'any')
       provider.on('network', handleChainChanged)
-      // const signer = provider.getSigner()
-      const signer = new Wallet(PRIVATE_KEY, provider)
+      let signer
+      if (autosign) {
+        signer = new Wallet(PRIVATE_KEY, provider)
+      } else {
+        signer = provider.getSigner()
+      }
       const { chainId } = await provider.getNetwork()
       setChainId(chainId)
       setProvider(provider)
