b8448d75e9d4d0a5cdf3b44a7c41fc8a682fb5f6
Nicholas Molnar <nicholas@xmtp.com>
2022-08-16 16:31:26 -0700

    Remove console.log

--- helpers/env.ts
+++ helpers/env.ts
@@ -1,6 +1,5 @@
 export const getEnv = (): 'dev' | 'production' | 'local' => {
   const envVar = process.env.NEXT_PUBLIC_XMTP_ENVIRONMENT
-  console.log(envVar)
   if (envVar === 'production') {
     return envVar
   }
