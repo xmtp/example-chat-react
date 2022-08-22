fd3bde094fa5b49e38a7ac5bf2a7f735e6b80db8
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-08-22 16:45:12 +0530

    adding a check for rendering the conversation list only when address is present

--- pages/dm/[recipientWalletAddr].tsx
+++ pages/dm/[recipientWalletAddr].tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { Conversation } from '../../components/Conversation'
 
 const ConversationPage: NextPage = () => {
   const router = useRouter()
   const recipientWalletAddr = router.query.recipientWalletAddr as string
-  return <Conversation recipientWalletAddr={recipientWalletAddr} />
+  const [walletAddr, setWalletAddr] = useState<string | undefined>()
+
+  useEffect(() => {
+    setWalletAddr(recipientWalletAddr)
+  }, [recipientWalletAddr])
+
+  if (!walletAddr) return <div />
+
+  return <Conversation recipientWalletAddr={walletAddr} />
 }
 
 export default ConversationPage
