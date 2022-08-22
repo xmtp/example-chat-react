4868d9e58b8ce94837da365e7334de1749285935
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-08-22 21:48:11 +0530

    changes to make dynamic routing work

--- components/ConversationsList.tsx
+++ components/ConversationsList.tsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { classNames, truncate, formatDate } from '../helpers'
-// import Link from 'next/link'
+import Link from 'next/link'
 import Address from './Address'
 import { useRouter } from 'next/router'
 import { Conversation } from '@xmtp/xmtp-js'
@@ -26,8 +26,8 @@ const getLatestMessage = (messages: Message[]): Message | null =>
 const ConversationTile = ({
   conversation,
   isSelected,
+  onClick,
 }: ConversationTileProps): JSX.Element | null => {
-  const router = useRouter()
   const { loading: isLoadingEns } = useEns(conversation.peerAddress)
   const { messages, loading: isLoadingConversation } = useConversation(
     conversation.peerAddress
@@ -35,14 +35,11 @@ const ConversationTile = ({
   const loading = isLoadingEns || isLoadingConversation
   const latestMessage = getLatestMessage(messages)
   const path = `/dm/${conversation.peerAddress}`
-
-  const onClick = () => {
-    router.push(path)
-  }
+  const linkRef = useRef<HTMLAnchorElement>(null)
 
   useEffect(() => {
     if (isSelected) {
-      onClick()
+      linkRef.current?.click()
     }
   }, [isSelected])
 
@@ -51,59 +48,59 @@ const ConversationTile = ({
   }
 
   return (
-    // <Link passHref href={path} key={conversation.peerAddress}>
-    <a onClick={onClick}>
-      <div
-        className={classNames(
-          'h-20',
-          'py-2',
-          'px-4',
-          'md:max-w-sm',
-          'mx-auto',
-          'bg-white',
-          'space-y-2',
-          'py-2',
-          'flex',
-          'items-center',
-          'space-y-0',
-          'space-x-4',
-          'border-b-2',
-          'border-gray-100',
-          'hover:bg-bt-100',
-          loading ? 'opacity-80' : 'opacity-100',
-          isSelected ? 'bg-bt-200' : null
-        )}
-      >
-        <Avatar peerAddress={conversation.peerAddress} />
-        <div className="py-4 sm:text-left text w-full">
-          <div className="grid-cols-2 grid">
-            <Address
-              address={conversation.peerAddress}
-              className="text-black text-lg md:text-md font-bold place-self-start"
-            />
-            <span
+    <Link href={path} key={conversation.peerAddress}>
+      <a ref={linkRef} onClick={onClick}>
+        <div
+          className={classNames(
+            'h-20',
+            'py-2',
+            'px-4',
+            'md:max-w-sm',
+            'mx-auto',
+            'bg-white',
+            'space-y-2',
+            'py-2',
+            'flex',
+            'items-center',
+            'space-y-0',
+            'space-x-4',
+            'border-b-2',
+            'border-gray-100',
+            'hover:bg-bt-100',
+            loading ? 'opacity-80' : 'opacity-100',
+            isSelected ? 'bg-bt-200' : null
+          )}
+        >
+          <Avatar peerAddress={conversation.peerAddress} />
+          <div className="py-4 sm:text-left text w-full">
+            <div className="grid-cols-2 grid">
+              <Address
+                address={conversation.peerAddress}
+                className="text-black text-lg md:text-md font-bold place-self-start"
+              />
+              <span
+                className={classNames(
+                  'text-lg md:text-sm font-normal place-self-end',
+                  isSelected ? 'text-n-500' : 'text-n-300',
+                  loading ? 'animate-pulse' : ''
+                )}
+              >
+                {formatDate(latestMessage?.sent)}
+              </span>
+            </div>
+            <p
               className={classNames(
-                'text-lg md:text-sm font-normal place-self-end',
+                'text-[13px] md:text-sm font-normal text-ellipsis mt-0',
                 isSelected ? 'text-n-500' : 'text-n-300',
                 loading ? 'animate-pulse' : ''
               )}
             >
-              {formatDate(latestMessage?.sent)}
-            </span>
+              {latestMessage && truncate(latestMessage.content, 75)}
+            </p>
           </div>
-          <p
-            className={classNames(
-              'text-[13px] md:text-sm font-normal text-ellipsis mt-0',
-              isSelected ? 'text-n-500' : 'text-n-300',
-              loading ? 'animate-pulse' : ''
-            )}
-          >
-            {latestMessage && truncate(latestMessage.content, 75)}
-          </p>
         </div>
-      </div>
-    </a>
-    /* </Link> */
+      </a>
+    </Link>
   )
 }
 
