ad42dca8414952cee547e593c4a6bce18d65ded0
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-08-22 17:45:04 +0530

    changes to make dynamic routing work

--- components/ConversationsList.tsx
+++ components/ConversationsList.tsx
@@ -26,7 +26,6 @@ const getLatestMessage = (messages: Message[]): Message | null =>
 const ConversationTile = ({
   conversation,
   isSelected,
-  onClick,
 }: ConversationTileProps): JSX.Element | null => {
   const router = useRouter()
   const { loading: isLoadingEns } = useEns(conversation.peerAddress)
@@ -37,10 +36,13 @@ const ConversationTile = ({
   const latestMessage = getLatestMessage(messages)
   const path = `/dm/${conversation.peerAddress}`
 
+  const onClick = () => {
+    router.push(path)
+  }
+
   useEffect(() => {
     if (isSelected) {
-      router.push(path)
-      onClick && onClick()
+      onClick()
     }
   }, [isSelected])
 
@@ -49,59 +51,59 @@ const ConversationTile = ({
   }
 
   return (
-    <Link passHref href={path} key={conversation.peerAddress}>
-      <a onClick={onClick}>
-        <div
-          className={classNames(
-            'h-20',
-            'py-2',
-            'px-4',
-            'md:max-w-sm',
-            'mx-auto',
-            'bg-white',
-            'space-y-2',
-            'py-2',
-            'flex',
-            'items-center',
-            'space-y-0',
-            'space-x-4',
-            'border-b-2',
-            'border-gray-100',
-            'hover:bg-bt-100',
-            loading ? 'opacity-80' : 'opacity-100',
-            isSelected ? 'bg-bt-200' : null
-          )}
-        >
-          <Avatar peerAddress={conversation.peerAddress} />
-          <div className="py-4 sm:text-left text w-full">
-            <div className="grid-cols-2 grid">
-              <Address
-                address={conversation.peerAddress}
-                className="text-black text-lg md:text-md font-bold place-self-start"
-              />
-              <span
-                className={classNames(
-                  'text-lg md:text-sm font-normal place-self-end',
-                  isSelected ? 'text-n-500' : 'text-n-300',
-                  loading ? 'animate-pulse' : ''
-                )}
-              >
-                {formatDate(latestMessage?.sent)}
-              </span>
-            </div>
-            <p
+    // <Link passHref href={path} key={conversation.peerAddress}>
+    <a onClick={onClick}>
+      <div
+        className={classNames(
+          'h-20',
+          'py-2',
+          'px-4',
+          'md:max-w-sm',
+          'mx-auto',
+          'bg-white',
+          'space-y-2',
+          'py-2',
+          'flex',
+          'items-center',
+          'space-y-0',
+          'space-x-4',
+          'border-b-2',
+          'border-gray-100',
+          'hover:bg-bt-100',
+          loading ? 'opacity-80' : 'opacity-100',
+          isSelected ? 'bg-bt-200' : null
+        )}
+      >
+        <Avatar peerAddress={conversation.peerAddress} />
+        <div className="py-4 sm:text-left text w-full">
+          <div className="grid-cols-2 grid">
+            <Address
+              address={conversation.peerAddress}
+              className="text-black text-lg md:text-md font-bold place-self-start"
+            />
+            <span
               className={classNames(
-                'text-[13px] md:text-sm font-normal text-ellipsis mt-0',
+                'text-lg md:text-sm font-normal place-self-end',
                 isSelected ? 'text-n-500' : 'text-n-300',
                 loading ? 'animate-pulse' : ''
               )}
             >
-              {latestMessage && truncate(latestMessage.content, 75)}
-            </p>
+              {formatDate(latestMessage?.sent)}
+            </span>
           </div>
+          <p
+            className={classNames(
+              'text-[13px] md:text-sm font-normal text-ellipsis mt-0',
+              isSelected ? 'text-n-500' : 'text-n-300',
+              loading ? 'animate-pulse' : ''
+            )}
+          >
+            {latestMessage && truncate(latestMessage.content, 75)}
+          </p>
         </div>
-      </a>
-    </Link>
+      </div>
+    </a>
+    /* </Link> */
   )
 }
 
