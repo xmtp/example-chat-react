809faa0d161892f71cc8590e805dcb029de06416
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-08-23 14:02:43 +0530

    fixing routing issue when directly browsing the URL

--- components/ConversationsList.tsx
+++ components/ConversationsList.tsx
@@ -8,7 +8,7 @@ import { XmtpContext } from '../contexts/xmtp'
 import { Message } from '@xmtp/xmtp-js'
 import useEns from '../hooks/useEns'
 import Avatar from './Avatar'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 type ConversationsListProps = {
   conversations: Conversation[]
@@ -112,6 +112,25 @@ const ConversationsList = ({
       getLatestMessage(convoBMessages)?.sent || new Date()
     return convoALastMessageDate < convoBLastMessageDate ? 1 : -1
   }
+
+  const [resetPage, setResetPage] = useState(false)
+
+  const reloadIfQueryParamPresent = () => {
+    if (window.location.pathname) {
+      const path = window.location.pathname.replace('/dm/', '')
+      const matchAddress = conversations.filter(
+        (convo) => path == convo.peerAddress
+      )
+      if (Array.isArray(matchAddress) && matchAddress.length > 0) {
+        setResetPage(!resetPage)
+      }
+    }
+  }
+
+  useEffect(() => {
+    reloadIfQueryParamPresent()
+  }, [window.location.pathname])
+
   return (
     <div>
       {conversations &&
