2fe9796071697afca48fdd929cedbdff0856df7f
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-09-06 16:12:48 +0530

    changes to make ens in url work

--- components/ConversationsList.tsx
+++ components/ConversationsList.tsx
@@ -5,11 +5,18 @@ import Address from './Address'
 import { useRouter } from 'next/router'
 import { Conversation } from '@xmtp/xmtp-js'
 import { Message } from '@xmtp/xmtp-js'
-import { classNames, truncate, formatDate, checkPath } from '../helpers'
+import {
+  classNames,
+  truncate,
+  formatDate,
+  checkPath,
+  checkIfPathIsEns,
+} from '../helpers'
 import useConversation from '../hooks/useConversation'
 import Avatar from './Avatar'
 import useMessageStore from '../hooks/useMessageStore'
 import XmtpContext from '../contexts/xmtp'
+import { WalletContext } from '../contexts/wallet'
 
 type ConversationTileProps = {
   conversation: Conversation
@@ -101,6 +108,7 @@ const ConversationsList = (): JSX.Element => {
   const router = useRouter()
   const { conversations } = useContext(XmtpContext)
   const { messageStore } = useMessageStore()
+  const { resolveName } = useContext(WalletContext)
 
   const orderByLatestMessage = (
     convoA: Conversation,
@@ -117,7 +125,10 @@ const ConversationsList = (): JSX.Element => {
 
   const reloadIfQueryParamPresent = async () => {
     if (checkPath()) {
-      const queryAddress = window.location.pathname.replace('/dm/', '')
+      let queryAddress = window.location.pathname.replace('/dm/', '')
+      if (checkIfPathIsEns(queryAddress)) {
+        queryAddress = (await resolveName(queryAddress)) ?? ''
+      }
       if (queryAddress) {
         if (conversations && conversations.has(queryAddress)) {
           router.push(window.location.pathname)
