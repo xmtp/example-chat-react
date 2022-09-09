347fb2f4d4e7b5dfcc90b4fe43433ede14fd0c16
antho1404 <anthony.estebe@gmail.com>
2022-09-10 04:52:36 +0700

    fetch on liteflow + ens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,34 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import useWallet from '../hooks/useWallet'
 import XmtpProvider from '../components/XmtpProvider'
+import { fetchEns, fetchLiteflow } from '../helpers/fetcher'
+import { useCallback } from 'react'
+
+const cache = new Map<string, Promise<{ name?: string; avatar?: string }>>()
 
 function AppWrapper({ Component, pageProps }: AppProps) {
   const signer = useWallet()
+
+  const lookup = useCallback(
+    (address) => {
+      const res = cache.get(address)
+      if (res) return res
+
+      const promise = Promise.all([
+        fetchLiteflow(address),
+        fetchEns(address, signer?.provider),
+      ]).then(([liteflow, ens]) => ({
+        name: liteflow.name || ens.name,
+        avatar: liteflow.avatar || ens.avatar,
+      }))
+
+      cache.set(address, promise)
+      return promise
+    },
+    [signer]
+  )
   return (
-    <XmtpProvider signer={signer}>
+    <XmtpProvider signer={signer} lookupAddress={lookup}>
       <Component {...pageProps} />
     </XmtpProvider>
   )
