a9b630a92a990e46432cef61cbf1aafcb6269380
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-11-16 08:12:00 +0100

    minor constant added

--- helpers/keys.ts
+++ helpers/keys.ts
@@ -1,17 +1,19 @@
 import { getEnv } from './env'
 
+const ENCODING = 'binary'
+
 export const buildLocalStorageKey = (walletAddress: string) =>
   `xmtp:${getEnv()}:keys:${walletAddress}`
 
 export const loadKeys = (walletAddress: string): Uint8Array | null => {
   const val = localStorage.getItem(buildLocalStorageKey(walletAddress))
-  return val ? Buffer.from(val, 'binary') : null
+  return val ? Buffer.from(val, ENCODING) : null
 }
 
 export const storeKeys = (walletAddress: string, keys: Uint8Array) => {
   localStorage.setItem(
     buildLocalStorageKey(walletAddress),
-    Buffer.from(keys).toString('binary')
+    Buffer.from(keys).toString(ENCODING)
   )
 }
 
