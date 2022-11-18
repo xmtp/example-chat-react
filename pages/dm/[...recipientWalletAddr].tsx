b8e5eb1fdaf605cb45a67128a43aa317813a3d2b
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-11-18 19:41:41 +0100

    make the ens routing work

--- pages/dm/[...recipientWalletAddr].tsx
+++ pages/dm/[...recipientWalletAddr].tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { Conversation } from '../../components/Conversation'
+import useWalletProvider from '../../hooks/useWalletProvider'
 
 const ConversationPage: NextPage = () => {
   const router = useRouter()
+  const { resolveName } = useWalletProvider()
   const recipientWalletAddr = Array.isArray(router.query.recipientWalletAddr)
     ? router.query.recipientWalletAddr.join('/')
     : router.query.recipientWalletAddr
+
+  useEffect(() => {
+    const checkIfEns = async () => {
+      if (recipientWalletAddr?.includes('.eth')) {
+        const address = await resolveName(recipientWalletAddr)
+        router.push(`/dm/${address}`)
+      }
+    }
+    checkIfEns()
+  }, [recipientWalletAddr])
 
   return <Conversation recipientWalletAddr={recipientWalletAddr ?? ''} />
 }
